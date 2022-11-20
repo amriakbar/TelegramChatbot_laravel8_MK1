6212c5b1f0d88d54111b2df986c8c701c71a7d4f
6h0stC0lf1n <37840294+amriakbar@users.noreply.github.com>
2022-11-20 09:47:00 +0000

    git pull

---
+++ app/Models/Api.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Api extends Authenticatable
+{
+  use HasApiTokens, HasFactory, Notifiable;
+}
